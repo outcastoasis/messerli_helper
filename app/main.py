98c82145d0f981de7270f03730b1bb093fa433a6
Jascha Bucher <jascha.bucher@rebsamen.net>
2026-04-22 11:13:18 +0200

    version 0.2.4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import importlib
 import sys
 
 from PySide6.QtGui import QIcon
@@ -18,7 +17,7 @@ from app.utils.paths import get_bundle_dir
 from app.utils.windows import set_current_process_app_user_model_id
 
 try:
-    pyi_splash = importlib.import_module("pyi_splash")
+    import pyi_splash
 except ImportError:  # pragma: no cover - available only in frozen builds with boot splash
     pyi_splash = None
 
